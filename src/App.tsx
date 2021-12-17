2882302768fdd97d8e318c9560945f841527f795
Tampoka <goal.thanks@gmail.com>
2021-12-17 14:07:54 +0900

    Fixed naming.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import Lesson3 from "./lessons/lesson3/Lesson3";
 function App() {
     return (
         <div className="container">
-            <Lesson1 />
             <Lesson3 />
             {/*<Lesson4 />*/}
         </div>
--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -139,36 +139,187 @@ export default () => {
 //     .then(res3 => {
 //         console.log('res3', res3)
 //     })
-    // .then(res4 => {
-    //     console.log('res4', res4)
-    // },
-    //         err => {
-    //             console.log('err', err)
-    //             return new Promise((resolve, reject) => {
-    //                 resolve({name: 'Ann', age: 33})
-    //             })
-    //         })
-    //
-    // .then(null,err=>{
-    //     console.log('err', err)
-    // })
-    // .catch(err=>{
-    //     console.log('err', err)
-    // })
-    //
+// .then(res4 => {
+//     console.log('res4', res4)
+// },
+//         err => {
+//             console.log('err', err)
+//             return new Promise((resolve, reject) => {
+//                 resolve({name: 'Ann', age: 33})
+//             })
+//         })
+//
+// .then(null,err=>{
+//     console.log('err', err)
+// })
+// .catch(err=>{
+//     console.log('err', err)
+// })
+//
 
-let prom: Promise<number> = new Promise((resolve, reject) => {
-    resolve(0)
-})
-prom
-    .then(res1 => {
-        console.log('res1', res1)
-    })
-    .then(res2 => {
-        console.log('res2', res2)
-    })
-    .then(res3 => {
-        console.log('res3', res3)
-    })
+// let prom: Promise<number> = new Promise((resolve, reject) => {
+//     resolve(0)
+// })
+// prom
+//     .then(res1 => {
+//         console.log('res1', res1)
+//     },err=>{
+//         console.log('err')
+//     })
+//     .then(res2 => {
+//         console.log('res2', res2)
+//     })
+//     .then(res3 => {
+//         console.log('res3', res3)
+//     })
+//
+
+// let prom: Promise<number> = new Promise((resolve, reject) => {
+//     resolve(0)
+// })
+// prom
+//     .then(res1 => {
+//         console.log('res1', res1)
+//     },err=>{
+//         console.log('err',err)
+//     })
+//     .then(res2 => {
+//         console.log('res2', res2)
+//     },err=>{
+//         console.log('err',err)
+//     })
+//     .then(null,err => {
+//         console.log('err',err)
+//     })
+
+// let prom: Promise<number> = new Promise((resolve, reject) => {
+//     resolve(0)
+// })
+// prom
+//     .then(res1 => {
+//         console.log('res1', res1)
+//     },err=>{
+//         console.log('err',err)
+//     })
+//     .then(res2 => {
+//         console.log('res2', res2)
+//     },err=>{
+//         console.log('err',err)
+//     })
+//     .then(null,err => {
+//         console.log('err',err)
+//     })
+
+// let prom: Promise<number> = new Promise((resolve, reject) => {
+//     resolve(0)
+// })
+// prom
+//     .then(res1 => {
+//         console.log('res1', res1)
+//         throw 50
+//     })
+//     .catch(err=>{
+//         console.log(err)
+//     })
+//     .then(res2 => {
+//         console.log('res2', res2)
+//     })
+//     .catch(err=> {
+//         console.log(err)
+//     })
+//     .then(res3 => {
+//         console.log('res3', res3)
+//     })
+//     .catch(err=> {
+//         console.log(err)
+//     })
+
+// let prom: Promise<number> = new Promise((resolve, reject) => {
+//     reject(-10)
+// })
+// prom
+//     .catch(err => {
+//         console.log(err)
+//     })
+//     .then(res1 => {
+//         console.log('res1', res1)
+//         throw 50
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
+//     .then(res2 => {
+//         console.log('res2', res2)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
+//     .then(res3 => {
+//         console.log('res3', res3)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
+
+// let prom: Promise<number> = new Promise((resolve, reject) => {
+//     reject(-10)
+// })
+//
+// prom
+//     .finally(()=> {
+//         console.log('res1 in finally')
+//     })
+//     .then(res1 => {
+//         console.log('res1', res1)
+//         throw 50
+//     })
+//     .finally(()=> {
+//         console.log('res1 in finally')
+//     })
+//     .catch(err => {
+//         console.log('err',err)
+//         return 1000
+//     })
+//     // @ts-ignore
+//     .finally(()=> {
+//         console.log('res1 in finally')
+//     })
+
+// let prom: Promise<number> = new Promise((resolve, reject) => {
+//     reject(-10)
+// })
+//
+// prom
+//     .finally(()=> {
+//        throw 100500
+//     })
+//     .then(res1 => {
+//         console.log('res1', res1)
+//         throw 50
+//     })
+//     .catch(err => {
+//         console.log('err',err)
+//         return 1000
+//     })
 
 
+// let prom: Promise<number> = new Promise((resolve, reject) => {
+//     setTimeout(()=>{
+//         resolve(10)
+//     },1000)
+// })
+//
+// prom.then(res=>{
+//     console.log(res)
+// })
+//     .catch(err=>{
+//         console.log(err)
+//     })
+
+// console.log('start')
+// Promise.resolve().then(()=>{
+//     console.log('Promise')})
+// console.log('end')
+
+let prom=new Promise(res=>res(10))
+let prom2=Promise.resolve(10)
